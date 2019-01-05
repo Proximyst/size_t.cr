5581750187027b4ec22cb4b9ce829b4aa0cd67e8
Mariell Hoversholm <proximyst@proximyst.com>
2019-01-05 14:04:13 +0100

    fix spec for unused but still existent archs

--- spec/size_t_spec.cr
+++ spec/size_t_spec.cr
@@ -2,7 +2,7 @@ require "./spec_helper"
 
 describe Sizes do
   it "size check" do
-    {% if flag?(:x86_64) || flag?(:aarch64) %}
+    {% if flag?(:x86_64) || flag?(:aarch64) || flag?(:mips64) || flag?(:mips64el) || flag?(:ppc64) || flag?(:nvptx64) || flag?(:spir64) %}
       Sizes::USize.should eq UInt64
       Sizes::ISize.should eq Int64
     {% else %}
